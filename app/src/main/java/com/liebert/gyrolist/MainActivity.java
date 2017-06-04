271d0b5259694926a0bacd4a701d2fc30d9d6f08
shorti1996 <shorti1996@gmail.com>
2017-06-04 23:02:44 +0200

    List contains apps

--- app/src/main/java/com/liebert/gyrolist/MainActivity.java
+++ app/src/main/java/com/liebert/gyrolist/MainActivity.java
@@ -1,6 +1,8 @@
 package com.liebert.gyrolist;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.graphics.drawable.BitmapDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,17 +11,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.util.Pair;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.bumptech.glide.util.Util;
 import com.liebert.gyrolist.Views.SquareImageView;
 
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -28,11 +28,12 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     private SensorManager mSensorManager;
-    private Sensor mRotationVectorSensor;
+    private Sensor mAccelerometer;
 
-    SensorEventListener rotationSensorEventListener;
+    SensorEventListener mAccelerometerEventListener;
 
-    List<Integer> placeholderList = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+//    List<Integer> placeholderList = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    List<ApplicationInfo> appList;
 
     @BindView(R.id.main_list_rv)
     RecyclerView mainRv;
@@ -42,18 +43,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        mRotationVectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        rotationSensorEventListener = new MyListener(this);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccelerometerEventListener = new MyAccelerometerListener(this);
 
         ButterKnife.bind(this);
 
-        setupList();
+        setupRv();
     }
 
-    class MyListener implements SensorEventListener {
+    class MyAccelerometerListener implements SensorEventListener {
         Context mContext;
 
-        public MyListener(Context mContext) {
+        public MyAccelerometerListener(Context mContext) {
             this.mContext = mContext;
         }
 
@@ -67,10 +68,11 @@ public class MainActivity extends AppCompatActivity {
                 float dir = ((float[]) p.first)[2];
 //                Log.d("Aaa:", "Angle: " + Math.toDegrees(angle) * dir);
 
-                if (angle <= 5) {
+                if (angle <= 20) {
                     return;
                 } else {
-                    angle = angle * angle / 5;
+                    angle = angle - 10f;
+                    angle = angle * angle / 2;
 
                 }
 
@@ -85,32 +87,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    void setupList() {
+    void setupRv() {
+        appList = getPackageManager().getInstalledApplications(0);
         LinearLayoutManager layoutManager
                 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         mainRv.setLayoutManager(layoutManager);
-        mainRv.setAdapter(new ItemsAdapter(this, placeholderList));
+        mainRv.setAdapter(new ItemsAdapter(this, appList, mainRv));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(rotationSensorEventListener, mRotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(mAccelerometerEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(rotationSensorEventListener);
+        mSensorManager.unregisterListener(mAccelerometerEventListener);
     }
 
-    class ItemsAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+    class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
         Context mContext;
-        List<Integer> mItemsList;
+        List<ApplicationInfo> mItemsList;
+        RecyclerView mRecyclerView;
 
-        public ItemsAdapter(Context context, List<Integer> items) {
-            mContext = context;
-            mItemsList = items;
+        public ItemsAdapter(Context mContext, List<ApplicationInfo> mItemsList, RecyclerView mRecyclerView) {
+            this.mContext = mContext;
+            this.mItemsList = mItemsList;
+            this.mRecyclerView = mRecyclerView;
         }
 
         @Override
@@ -124,21 +129,34 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(ItemViewHolder holder, int position) {
 //            holder.itemSiv;
+            ApplicationInfo applicationInfo = appList.get(position);
+            BitmapDrawable icon = (BitmapDrawable)applicationInfo.loadIcon(getPackageManager());
+            holder.itemSiv.setImageBitmap(icon.getBitmap());
+            holder.titleTv.setText(applicationInfo.loadLabel(getPackageManager()));
         }
 
         @Override
         public int getItemCount() {
             return mItemsList.size();
         }
-    }
 
-    class ItemViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.item_icon)
-        SquareImageView itemSiv;
+        class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            @BindView(R.id.item_icon)
+            SquareImageView itemSiv;
 
-        public ItemViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
+            @BindView(R.id.item_title)
+            TextView titleTv;
+
+            public ItemViewHolder(View itemView) {
+                super(itemView);
+                ButterKnife.bind(this, itemView);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, titleTv.getText(), Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
